784b5ec726a1012f072f118135013d766edd8c24
chujunjie <c.junjie@hotmail.com>
2018-10-07 16:32:55 +0800

    TCPSocket

---
+++ tcp/TCPClient.py
@@ -0,0 +1,13 @@
+from socket import *
+
+serverName = 'localhost'
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_STREAM)  # AF_INET表示底层网络使用IPv4，SOCK_STREAM表示一个TCP套接字
+clientSocket.connect((serverName, serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(sentence.encode())
+
+modifiedSentence = clientSocket.recv(1024)
+print('From Server: ', modifiedSentence.decode())
+clientSocket.close()
